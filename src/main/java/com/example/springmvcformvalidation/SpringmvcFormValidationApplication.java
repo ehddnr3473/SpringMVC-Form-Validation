c02d9935412489ab7eab4fc99e29e28b3d917c70
김동욱 <ehddnr73@naver.com>
2025-11-13 13:54:58 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/springmvcformvalidation/SpringmvcFormValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcformvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcFormValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcFormValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmvcformvalidation/SpringmvcFormValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcformvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcFormValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
